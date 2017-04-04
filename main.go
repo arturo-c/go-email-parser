7e216481777485eff8d28478dcbfc116a6f22fd1
art kontreras <arturo.kontreras@gmail.com>
2017-04-03 22:13:30 -0500

    Rework 1

--- main.go
+++ main.go
@@ -1,47 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"bytes"
-	"fmt"
-	"net/mail"
-	"github.com/gorilla/mux"
-	"io/ioutil"
-)
-
-type EmailJson struct {
-	To string `json:"To"`
-	From string `json:"From"`
-	Date string `json:"Date"`
-	Subject string `json:"Subject"`
-	MessageID string `json:"Message-ID"`
-}
-
-func ParseEmailEndpoint(w http.ResponseWriter, req *http.Request) {
-	//params  := mux.Vars(req)
-	body, err := ioutil.ReadAll(req.Body)
-	msg, err := mail.ReadMessage(bytes.NewBuffer([]byte(body)))
-	var emailJson EmailJson
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(msg.Header.Get("Message-ID"))
-	emailJson.To = msg.Header.Get("To")
-	emailJson.From = msg.Header.Get("From")
-	emailJson.Date = msg.Header.Get("Date")
-	emailJson.Subject = msg.Header.Get("Subject")
-	emailJson.MessageID = msg.Header.Get("Message-ID")
-
-	w.Header().Set("Content-Type", "application/json")
-	enc := json.NewEncoder(w)
-	enc.SetEscapeHTML(false)
-	enc.Encode(emailJson)
-}
-
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/parse", ParseEmailEndpoint).Methods("POST")
-	log.Fatal(http.ListenAndServe(":80", router))
+	a := App{}
+	a.Initialize()
+
+	a.Run(":80")
 }
